a7bb707593895441c59db9cead45269b4222b66d
rodrigomuniz <rodrigo.temiski1995@gmail.com>
2014-04-16 21:05:07 -0300

    sqlite

--- src/main/java/com/tcc/viralata/HomeController.java
+++ src/main/java/com/tcc/viralata/HomeController.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.postgresql.*;
 
 /**
  * Handles requests for the application home page.
@@ -46,9 +45,9 @@ public class HomeController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(String login, String senha) throws ClassNotFoundException {
-		Class.forName("org.postgresql.Driver");
+		Class.forName("org.sqlite.JDBC");
 		try {
-			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/rodrigomuniz", "rodrigomuniz", "");
+			Connection con = DriverManager.getConnection("jdbc:sqlite:viralata.db", "rodrigomuniz", "");
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM usuario where Login="+login+" AND Senha="+senha);
 		} catch (SQLException e) {
